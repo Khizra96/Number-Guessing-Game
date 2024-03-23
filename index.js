f5185429859fc7b3c1c5360cb0741251226fab90
khizra tahir <khizratahir9@gmail.com>
2024-03-23 11:33:00 -0700

    Created Number Guess Game

---
+++ index.js
@@ -0,0 +1,15 @@
+import inquirer from "inquirer";
+const randomnumber = Math.floor(Math.random() * 6 + 1);
+const NumGuess = await inquirer.prompt([
+    {
+        name: "NumberGuess",
+        type: "number",
+        message: "Select any one Number between 1 to 6",
+    },
+]);
+if (NumGuess.userGuessnumber === randomnumber) {
+    console.log("Congrates You Guess Right Number");
+}
+else {
+    console.log("you Guess Wrong Number");
+}
